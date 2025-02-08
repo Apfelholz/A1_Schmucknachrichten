dd0024034ccd0f6657f05a8da60a909cbe905fef
Apfelholz <apfelholzz1@gmail.com>
2025-02-08 13:38:05 +0100

    feat: update main method to read data from file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String dateipfad = "data\\schmuck1.txt";
+        
+        int numberOfDifferentPearlTypes = FileReaderx.readLineToInt(dateipfad, 0);
+
+        int[] pearlTypes = FileReaderx.readToInt2DArray(dateipfad, 1, 1)[0];
+
+        char[] message = FileReaderx.readToContinuousCharArray(dateipfad, 2, -1);
+
+        System.out.println("suscess");
     }
 }
