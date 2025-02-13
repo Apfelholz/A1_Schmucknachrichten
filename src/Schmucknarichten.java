8d8403fd643bc19cae10a3d87ea588ef71cda3fe
Apfelholz <apfelholzz1@gmail.com>
2025-02-13 17:26:39 +0100

    feat: enhance Schmucknarichten to accept file path as argument and improve output formatting

--- src/Schmucknarichten.java
+++ src/Schmucknarichten.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class Schmucknarichten {
     public static void main(String[] args) throws Exception {
         
-        String dateipfad = "data\\schmuck0.txt";
+        String dateipfad = args[0];
         
         int numberOfDifferentPearlTypes = FileReaderx.readLineToInt(dateipfad, 0);
 
@@ -19,19 +19,26 @@ public class Schmucknarichten {
         HuffmanKodierungNonBinary huffmanKodierungnNonBinary = new HuffmanKodierungNonBinary();
         HashMap<Character,String> codeMap = huffmanKodierungnNonBinary.huffmanKodierung(message, numberOfDifferentPearlTypes);
 
-        for (Character key : codeMap.keySet()) {
-            System.out.println(key + ": " + codeMap.get(key));
-        }
-
         String messageCode = "";
-        int messageLegth = 0;
+        int messageLength = 0;
         for (char c : message){
             messageCode = messageCode + codeMap.get(c);
         }
         for (char m : messageCode.toCharArray()){
-            messageLegth += pearlTypeMap.get(Integer.parseInt(Character.toString(m)));
+            messageLength += pearlTypeMap.get(Integer.parseInt(Character.toString(m)));
         }
-        System.out.println("The encoded message: " + messageCode);
-        System.out.println("The length of the encoded message: " + messageLegth);
+
+        // Capture the required values
+        String shortenedMessageCode = messageCode.length() > 40 ? messageCode.substring(0, 40) + "..." : messageCode;
+        String shortenedMessage = new String(message).length() > 40 ? new String(message).substring(0, 40) + "..." : new String(message);
+        String shortenedPearlTypeMap = pearlTypeMap.toString().length() > 100 ? pearlTypeMap.toString().substring(0, 100) + "..." : pearlTypeMap.toString();
+        String codeMapRepresentation = codeMap.toString().length() > 100 ? codeMap.toString().substring(0, 100) + "..." : codeMap.toString();
+
+        // Print the captured values
+        System.out.println("First 40 characters of messageCode: " + shortenedMessageCode);
+        System.out.println("First 40 characters of message: " + shortenedMessage);
+        System.out.println("Shortened pearlTypeMap: " + shortenedPearlTypeMap);
+        System.out.println("CodeMap: " + codeMapRepresentation);
+        System.out.println("Length of the encoded message: " + messageLength);
     }
 }
