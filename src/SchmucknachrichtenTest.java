fa9e7e603d7d27b22f5940cc36c9495472309b93
Apfelholz <149402866+Apfelholz@users.noreply.github.com>
2025-02-28 20:09:22 +0100

    fix: use relative Paths

--- src/SchmucknachrichtenTest.java
+++ src/SchmucknachrichtenTest.java
@@ -6,62 +6,62 @@ import java.io.PrintStream;
 public class SchmucknachrichtenTest {
     @Test
     void testMainWithSchmuck0() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck0.txt");
+        testMainWithFile("./data/schmuck0.txt");
     }
 
     @Test
     void testMainWithSchmuck00() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck00.txt");
+        testMainWithFile("./data/schmuck00.txt");
     }
 
     @Test
     void testMainWithSchmuck01() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck01.txt");
+        testMainWithFile("./data/schmuck01.txt");
     }
 
     @Test
     void testMainWithSchmuck1() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck1.txt"); 
+        testMainWithFile("./data/schmuck1.txt"); 
     }
 
     @Test
     void testMainWithSchmuck2() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck2.txt");
+        testMainWithFile("./data/schmuck2.txt");
     }
 
     @Test
     void testMainWithSchmuck3() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck3.txt");
+        testMainWithFile("./data/schmuck3.txt");
     }
 
     @Test
     void testMainWithSchmuck4() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck4.txt");
+        testMainWithFile("./data/schmuck4.txt");
     }
 
     @Test
     void testMainWithSchmuck5() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck5.txt");
+        testMainWithFile("./data/schmuck5.txt");
     }
 
     @Test
     void testMainWithSchmuck6() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck6.txt");
+        testMainWithFile("./data/schmuck6.txt");
     }
 
     @Test
     void testMainWithSchmuck7() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck7.txt");
+        testMainWithFile("./data/schmuck7.txt");
     }
 
     @Test
     void testMainWithSchmuck8() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck8.txt");
+        testMainWithFile("./data/schmuck8.txt");
     }
 
     @Test
     void testMainWithSchmuck9() {
-        testMainWithFile("/d:/Documents/Programiren/Informatik Wettbewerb/BWINF 2024/Runde 2/A1_Schmucknachrichten/data/schmuck9.txt");
+        testMainWithFile("./data/schmuck9.txt");
     }
     
     private void testMainWithFile(String filePath) {
