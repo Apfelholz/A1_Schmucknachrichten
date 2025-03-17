e46298531b4088ff8d1130ca8e108a728b30c182
Apfelholz <apfelholzz1@gmail.com>
2025-03-17 16:11:57 +0100

    fix: working declaration of split codes

--- src/SchmucknachrichtenILP.java
+++ src/SchmucknachrichtenILP.java
@@ -19,12 +19,13 @@ public class SchmucknachrichtenILP {
 
     public static HashMap<Character, String> findCodes(char[] massage, int numberOfDifferentPearlTypes, int[] pearlTypes) {
         Loader.loadNativeLibraries();
-
-        MPSolver solver = new MPSolver("ILP", MPSolver.OptimizationProblemType.CBC_MIXED_INTEGER_PROGRAMMING);
-
+    
+        MPSolver solver = MPSolver.createSolver("SCIP");
+    
         HashMap<Character, Integer> charFrequencyMap = getCharTypesWithFrequency(massage);
         char[] charTypes = new char[charFrequencyMap.size()];
         int[] charfrequency = new int[charFrequencyMap.size()];
+    
         int idx = 0;
         for (int freq : charFrequencyMap.values()) {
             charfrequency[idx++] = freq;
@@ -33,82 +34,56 @@ public class SchmucknachrichtenILP {
         for (Character c : charFrequencyMap.keySet()) {
             charTypes[index++] = c;
         }
-
+    
+        // Declare MP VARs
         MPVariable[] codes = new MPVariable[charTypes.length];
         for (int i = 0; i < codes.length; i++) {
-            codes[i] = solver.makeIntVar(0, Integer.MAX_VALUE, Character.toString(charTypes[i]));
+            codes[i] = solver.makeIntVar(0.0, Integer.MAX_VALUE, Character.toString(charTypes[i]));
         }
-
+    
         MPVariable[][] splitCodes = new MPVariable[codes.length][10];
         for (int i = 0; i < codes.length; i++) {
             for (int j = 0; j < 10; j++) {
-                splitCodes[i][j] = solver.makeIntVar(0, numberOfDifferentPearlTypes - 1, Character.toString(charTypes[i]) + Integer.toString(j));
+                splitCodes[i][j] = solver.makeIntVar(0, numberOfDifferentPearlTypes - 1, "splitCodes_" + i + "_" + j);
             }
         }
-
-        MPVariable schmuckLength = solver.makeIntVar(0, Integer.MAX_VALUE, "schmuckLength");
-
-        // Implementiere das Setzen von splitCodes
-        for (int i = 0; i < codes.length; i++) {
-            MPConstraint constraint = solver.makeConstraint(codes[i].lb(), codes[i].ub(), "Split_Constraint_" + i);
-            for (int j = 0; j < 10; j++) {
-                constraint.setCoefficient(splitCodes[i][j], Math.pow(10, 9 - j));
+    
+        // Set constraints
+        for (int c = 0; c < codes.length; c++) {
+            MPConstraint constraint = solver.makeConstraint(0, 0, "constraint_" + c);
+    
+            // Für jede Ziffer in splitCodes, multipliziere mit der entsprechenden Potenz von 10
+            for (int sc = 0; sc < 10; sc++) {
+                constraint.setCoefficient(splitCodes[c][sc], (int) Math.pow(10, 9 - sc));
             }
+    
+            // Setze codes[c] gleich der Summe der Ziffern
+            constraint.setCoefficient(codes[c], -1);
         }
 
-        // Implementiere das Prefix-free-Speichern
-        for (int i = 0; i < codes.length; i++) {
-            for (int j = 0; j < codes.length; j++) {
-                if (i != j) {
-                    for (int k = 0; k < 10; k++) {
-                        MPConstraint constraint = solver.makeConstraint(0, 0, "prefixfree_Constraint_" + i + "_" + j + "_" + k);
-                        constraint.setCoefficient(splitCodes[i][k], 1);
-                        constraint.setCoefficient(splitCodes[j][k], -1);
-                    }
-                }
-            }
-        }
-
-        // Implementiere das Setzen der Länge
-        MPConstraint lengthConstraint = solver.makeConstraint(0, Integer.MAX_VALUE, "Length_Constraint");
-
-        for (int i = 0; i < codes.length; i++) {
-            for (int j = 0; j < 10; j++) {
-                lengthConstraint.setCoefficient(splitCodes[i][j], pearlTypes[(int) splitCodes[i][j].solutionValue()]*charfrequency[i]);
-            }
-        }
-
-        // Bedingung, um sicherzustellen, dass jeder Code einzigartig ist
-        for (int i = 0; i < codes.length; i++) {
-            for (int j = i + 1; j < codes.length; j++) {
-                MPConstraint uniqueConstraint = solver.makeConstraint(1, Double.POSITIVE_INFINITY, "Unique_Constraint_" + i + "_" + j);
-                uniqueConstraint.setCoefficient(codes[i], 1);
-                uniqueConstraint.setCoefficient(codes[j], -1);
-            }
-        }
-
-        // Setze das Ziel auf Minimierung der Gesamtlänge
+        
+    
         MPObjective objective = solver.objective();
-        objective.setCoefficient(schmuckLength, 1);
-        objective.setMinimization();
-
+        objective.setCoefficient(codes[0], 1);  // Maximierung von codes[0] oder eine andere Variable
+        objective.setMaximization();
+    
         if (solver.solve() == MPSolver.ResultStatus.OPTIMAL) {
-            System.out.println("optimal!");
+            System.out.println("Optimal!");
         } else {
             System.out.println("Kein Ergebnis gefunden.");
         }
-
+    
         // Schreibe die Lösungen aller Variablen in Arrays
         int[] codeValues = new int[codes.length];
         int[][] splitCodeValues = new int[codes.length][10];
-
+    
         for (int i = 0; i < codes.length; i++) {
             codeValues[i] = (int) codes[i].solutionValue();
             for (int j = 0; j < 10; j++) {
-            splitCodeValues[i][j] = (int) splitCodes[i][j].solutionValue();
+                splitCodeValues[i][j] = (int) splitCodes[i][j].solutionValue();
             }
         }
-
+    
         // Optional: Ausgabe der Werte zur Überprüfung
         System.out.println("Code Values:");
         for (int value : codeValues) {
@@ -117,16 +92,17 @@ public class SchmucknachrichtenILP {
         System.out.println("\nSplit Code Values:");
         for (int i = 0; i < splitCodeValues.length; i++) {
             for (int j = 0; j < splitCodeValues[i].length; j++) {
-            System.out.print(splitCodeValues[i][j] + " ");
+                System.out.print(splitCodeValues[i][j] + " ");
             }
             System.out.println();
         }
-
+    
         HashMap<Character, String> codeMap = new HashMap<>();
         for (int i = 0; i < codes.length; i++) {
-            codeMap.put(charTypes[i], Integer.toString((int) codes[i].solutionValue()));
+            codeMap.put(charTypes[i], Integer.toString(codeValues[i]));
         }
-
-         return codeMap;
+    
+        return codeMap;
     }
+    
 }
